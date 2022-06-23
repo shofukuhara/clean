6abf203b330e38f14c9080b91129ed70dbc5221a
shofukuhara <sho.fukuhara0213@gmail.com>
2022-06-23 22:55:43 +0900

    add: Tablet implementation

--- src/js/main.js
+++ src/js/main.js
@@ -4,91 +4,200 @@ new simpleParallax(image, {
   transition: "cubic-bezier(0,0,0,1)",
 });
 
-gsap.fromTo(
-  ".about__sectitle-bg",
-  {
-    x: "600px",
-  },
-  {
-    x: "0",
-    scrollTrigger: {
-      trigger: ".about__sectitle-bg",
-      start: "top center+=160",
-      end: "bottom center",
-      scrub: 0.8,
-      delay: 0.6,
-      // markers: true,
-    },
-  }
-);
-
-gsap.fromTo(
-  ".service__sectitle-bg",
-  {
-    x: "480px",
-  },
-  {
-    x: "0",
-    scrollTrigger: {
-      trigger: ".service__sectitle-bg",
-      start: "top center+=160",
-      end: "bottom center",
-      scrub: 0.8,
-      // markers: true,
-    },
-  }
-);
-
-gsap.fromTo(
-  ".news__sectitle-bg",
-  {
-    x: "560px",
-  },
-  {
-    x: "0",
-    scrollTrigger: {
-      trigger: ".news__sectitle-bg",
-      start: "top center+=160",
-      end: "bottom center",
-      scrub: 0.8,
-      // markers: true,
-    },
-  }
-);
-
-gsap.fromTo(
-  ".company__sectitle-bg",
-  {
-    x: "300px",
-  },
-  {
-    x: "0",
-    scrollTrigger: {
-      trigger: ".company__sectitle-bg",
-      start: "top center+=160",
-      end: "bottom center",
-      scrub: 0.8,
-      // markers: true,
-    },
-  }
-);
-
-const pop01 = ".pop01";
-// const start01 = 1000;
-
-gsap.set(pop01, {
-  opacity: 0,
-  top: "10px",
-  left: "60%",
-  markers: true,
+jQuery(function ($) {
+  $(".header__hamburger").on("click", function (e) {
+    e.stopPropagation();
+    $(this).toggleClass("active");
+    $(".header__list").toggleClass("active", $(this).hasClass("active"));
+  });
+  $(".header__list").on("click", function () {
+    $(".header__hamburger").click();
+  });
 });
-gsap.to(pop01, {
-  scrollTrigger: {
-    trigger: pop01,
-    start: "top center+=100",
-    end: "bottom center",
-    // markers: true,
+
+ScrollTrigger.matchMedia({
+  // 1025px以上
+  "(min-width: 1025px)": function () {
+    gsap.fromTo(
+      ".about__sectitle-bg",
+      {
+        x: "600px",
+      },
+      {
+        x: "0",
+        scrollTrigger: {
+          trigger: ".about__sectitle-bg",
+          start: "top center+=160",
+          end: "bottom center",
+          scrub: 0.8,
+          delay: 0.6,
+          // markers: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".service__sectitle-bg",
+      {
+        x: "480px",
+      },
+      {
+        x: "0",
+        scrollTrigger: {
+          trigger: ".service__sectitle-bg",
+          start: "top center+=160",
+          end: "bottom center",
+          scrub: 0.8,
+          // markers: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".news__sectitle-bg",
+      {
+        x: "560px",
+      },
+      {
+        x: "0",
+        scrollTrigger: {
+          trigger: ".news__sectitle-bg",
+          start: "top center+=160",
+          end: "bottom center",
+          scrub: 0.8,
+          // markers: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".company__sectitle-bg",
+      {
+        x: "300px",
+      },
+      {
+        x: "0",
+        scrollTrigger: {
+          trigger: ".company__sectitle-bg",
+          start: "top center+=160",
+          end: "bottom center",
+          scrub: 0.8,
+          // markers: true,
+        },
+      }
+    );
+
+    const pop01 = ".pop01";
+    // const start01 = 1000;
+    gsap.set(pop01, {
+      opacity: 0,
+      top: "10px",
+      left: "60%",
+      markers: true,
+    });
+    gsap.to(pop01, {
+      scrollTrigger: {
+        trigger: pop01,
+        start: "top center+=100",
+        end: "bottom center",
+        // markers: true,
+      },
+      duration: 1,
+      opacity: 1,
+    });
   },
-  duration: 1,
-  opacity: 1,
+
+  // 600px以上1025px以下
+  "(min-width: 600px) and (max-width: 1025px)": function () {
+    gsap.fromTo(
+      ".about__sectitle-bg",
+      {
+        x: "100px",
+      },
+      {
+        x: "0",
+        scrollTrigger: {
+          trigger: ".about__sectitle-bg",
+          start: "top center+=160",
+          end: "bottom center",
+          scrub: 0.8,
+          delay: 0.6,
+          // markers: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".service__sectitle-bg",
+      {
+        x: "80px",
+      },
+      {
+        x: "0",
+        scrollTrigger: {
+          trigger: ".service__sectitle-bg",
+          start: "top center+=160",
+          end: "bottom center",
+          scrub: 0.8,
+          // markers: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".news__sectitle-bg",
+      {
+        x: "100px",
+      },
+      {
+        x: "0",
+        scrollTrigger: {
+          trigger: ".news__sectitle-bg",
+          start: "top center+=160",
+          end: "bottom center",
+          scrub: 0.8,
+          // markers: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".company__sectitle-bg",
+      {
+        x: "60px",
+      },
+      {
+        x: "0",
+        scrollTrigger: {
+          trigger: ".company__sectitle-bg",
+          start: "top center+=160",
+          end: "bottom center",
+          scrub: 0.8,
+          // markers: true,
+        },
+      }
+    );
+
+    const pop01 = ".pop01";
+    // const start01 = 1000;
+    gsap.set(pop01, {
+      opacity: 0,
+      top: "10px",
+      left: "60%",
+      markers: true,
+    });
+    gsap.to(pop01, {
+      scrollTrigger: {
+        trigger: pop01,
+        start: "top center+=100",
+        end: "bottom center",
+        // markers: true,
+      },
+      duration: 1,
+      opacity: 1,
+    });
+  },
+
+  // 599px以下
+  "(max-width: 599px)": function () {},
 });
